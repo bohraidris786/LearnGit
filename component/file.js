803e705d1148f99a016346330534cb66143bbb61
idris bohra <idrisbohra786@gmail.com>
2023-05-24 10:15:22 +0530

    second commit

--- component/file.js
+++ component/file.js
@@ -1,15 +1,4 @@
 console.log('hello world');
-console.log('hello world');
-console.log('hello world');
-console.log('hello world');
-console.log('hello world');
-console.log('hello world');
-console.log('hello world');
-console.log('hello world');
-console.log('hello world');
-console.log('hello world');
-console.log('hello world');
-
 let idris = [1,2,3,4];
 for (let index = 0; index < idris.length; index++) {
     const element = idris[index];
