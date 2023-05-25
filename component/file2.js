caac6c70f284c1e34a9a4a55e63e1e99b6d666a4
idris bohra <idrisbohra5353@gmail.com>
2023-05-25 22:49:48 +0530

    for loops added in main branch

--- component/file2.js
+++ component/file2.js
@@ -1,6 +1,21 @@
 console.log('newidris branch has been created2')
 
+for (let index = 0; index < idris.length; index++) {
+    const element = idris[index];   
+}
+for (let index = 0; index < idris.length; index++) {
+    const element = idris[index];   
+}
+for (let index = 0; index < idris.length; index++) {
+    const element = idris[index];   
+}
+for (let index = 0; index < idris.length; index++) {
+    const element = idris[index];   
+}
 let idris = [1,2,3,4];
+for (let index = 0; index < idris.length; index++) {
+    const element = idris[index];   
+}
 for (let index = 0; index < idris.length; index++) {
     const element = idris[index];   
 }
