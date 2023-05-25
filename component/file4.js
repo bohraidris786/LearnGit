c41e641221a795dd28c2739b411b41c7b5eb7ec1
idris bohra <idrisbohra5353@gmail.com>
2023-05-26 00:41:30 +0530

    new file2

--- component/file4.js
+++ component/file4.js
@@ -1,3 +1,39 @@
 [1,2,3,4].forEach(element => {
     console.log('hello',element);
 });
+[1,2,3,4].forEach(element => {
+    console.log('hello',element);
+});
+[1,2,3,4].forEach(element => {
+    console.log('hello',element);
+});
+[1,2,3,4].forEach(element => {
+    console.log('hello',element);
+});
+[1,2,3,4].forEach(element => {
+    console.log('hello',element);
+});
+[1,2,3,4].forEach(element => {
+    console.log('hello',element);
+});
+[1,2,3,4].forEach(element => {
+    console.log('hello',element);
+});
+[1,2,3,4].forEach(element => {
+    console.log('hello',element);
+});
+[1,2,3,4].forEach(element => {
+    console.log('hello',element);
+});
+[1,2,3,4].forEach(element => {
+    console.log('hello',element);
+});
+[1,2,3,4].forEach(element => {
+    console.log('hello',element);
+});
+[1,2,3,4].forEach(element => {
+    console.log('hello',element);
+});
+[1,2,3,4].forEach(element => {
+    console.log('hello',element);
+});
